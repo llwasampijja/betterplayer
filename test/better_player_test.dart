53dd2191a1ace55ef31307734cde52a9893c6fd4
Lamech Lwasampijja <llwasampijja@gmail.com>
2024-11-08 11:24:10 +0300

    adds better player code and app runs well

--- test/better_player_test.dart
+++ test/better_player_test.dart
@@ -1,29 +1,44 @@
-import 'package:flutter_test/flutter_test.dart';
 import 'package:better_player/better_player.dart';
-import 'package:better_player/better_player_platform_interface.dart';
-import 'package:better_player/better_player_method_channel.dart';
-import 'package:plugin_platform_interface/plugin_platform_interface.dart';
-
-class MockBetterPlayerPlatform
-    with MockPlatformInterfaceMixin
-    implements BetterPlayerPlatform {
-
-  @override
-  Future<String?> getPlatformVersion() => Future.value('42');
-}
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:visibility_detector/visibility_detector.dart';
+import 'better_player_mock_controller.dart';
+import 'better_player_test_utils.dart';
 
 void main() {
-  final BetterPlayerPlatform initialPlatform = BetterPlayerPlatform.instance;
-
-  test('$MethodChannelBetterPlayer is the default instance', () {
-    expect(initialPlatform, isInstanceOf<MethodChannelBetterPlayer>());
+  setUpAll(() {
+    VisibilityDetectorController.instance.updateInterval = Duration.zero;
   });
 
-  test('getPlatformVersion', () async {
-    BetterPlayer betterPlayerPlugin = BetterPlayer();
-    MockBetterPlayerPlatform fakePlatform = MockBetterPlayerPlatform();
-    BetterPlayerPlatform.instance = fakePlatform;
-
-    expect(await betterPlayerPlugin.getPlatformVersion(), '42');
+  testWidgets("Better Player simple player - network",
+      (WidgetTester tester) async {
+    await tester.pumpWidget(_wrapWidget(
+        BetterPlayer.network(BetterPlayerTestUtils.bugBuckBunnyVideoUrl)));
+    expect(find.byWidgetPredicate((widget) => widget is BetterPlayer),
+        findsOneWidget);
   });
+
+  testWidgets("Better Player simple player - file",
+      (WidgetTester tester) async {
+    await tester.pumpWidget(_wrapWidget(
+        BetterPlayer.network(BetterPlayerTestUtils.bugBuckBunnyVideoUrl)));
+    expect(find.byWidgetPredicate((widget) => widget is BetterPlayer),
+        findsOneWidget);
+  });
+
+  testWidgets("BetterPlayer - with controller", (WidgetTester tester) async {
+    final BetterPlayerMockController betterPlayerController =
+        BetterPlayerMockController(const BetterPlayerConfiguration());
+    await tester.pumpWidget(_wrapWidget(BetterPlayer(
+      controller: betterPlayerController,
+    )));
+    expect(find.byWidgetPredicate((widget) => widget is BetterPlayer),
+        findsOneWidget);
+  });
+}
+
+///Wrap widget with material app to handle all features like navigation and
+///localization properly.
+Widget _wrapWidget(Widget widget) {
+  return MaterialApp(home: widget);
 }
